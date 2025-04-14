61e39d6a0e89d7b7c8351d0c9ecba7f8458f769f
JPaigna <126860913+JPaigna@users.noreply.github.com>
2025-04-14 23:40:44 +0800

    mylabs7 is u

--- models.py
+++ models.py
@@ -5,7 +5,6 @@ from database import Base
 class Book(Base):
     __tablename__ = "books"
 
-
-id = Column(Integer, primary_key=True, index=True)
-title = Column(String, index=True)
-author = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String)
